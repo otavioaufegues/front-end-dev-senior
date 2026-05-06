eff3c3415f4a00e0fbe7691ed304f1091e6ec85c
Otavio Augusto <otavioaufegues@gmail.com>
2026-05-06 17:02:58 -0300

    feat: models

---
+++ api/app/Models/Status.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Status extends Model
+{
+    protected $fillable = [
+        'key',
+        'name',
+        'position',
+    ];
+
+    public function tasks(): HasMany
+    {
+        return $this->hasMany(Task::class);
+    }
+}
+
