92074a73be016414dc6ccdfb25b7231109edba45
Otavio Augusto <otavioaufegues@gmail.com>
2026-05-06 15:17:30 -0300

    feat: criando primeira rota api

--- api/routes/web.php
+++ api/routes/web.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/api/mock-data', function () {
+    return response()->json([
+        'data' => [
+            ['id' => 1, 'name' => 'Todo 1', 'role' => 'Frontend'],
+            ['id' => 2, 'name' => 'Todo 2', 'role' => 'Backend'],
+        ],
+    ]);
 });
