dc4509741551b62f7870dd59e09e66a0f134befc
Otavio Augusto <otavioaufegues@gmail.com>
2026-05-06 20:08:11 -0300

    feat: axios services

---
+++ frontend/src/services/api/boards.ts
@@ -0,0 +1,26 @@
+import { getJson } from '../axios/axios'
+
+export type BoardStats = {
+  todo: number
+  in_progress: number
+  done: number
+}
+
+export type Board = {
+  id: number
+  name: string
+  description: string | null
+  theme_color: string
+  icon: string
+  last_activity_at: string | null
+  tasks_count_total: number
+  tasks_count_by_status: BoardStats
+}
+
+type ListBoardsResponse = { data: Board[] }
+
+export async function listBoards(): Promise<Board[]> {
+  const payload = await getJson<ListBoardsResponse>('/boards')
+  return payload.data
+}
+
